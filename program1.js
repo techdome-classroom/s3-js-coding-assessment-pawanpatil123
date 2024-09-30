7b8c0436da3fecefcfcf03b9d24d9ec3a95b829a
piyush patil <piyushpatil44099@gmail.com>
2024-09-30 15:52:04 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,7 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+function isValid(s) {
     let stack = [];
     let map = {
         ')': '(',
@@ -14,12 +14,9 @@ var isValid = function(s) {
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
         
-
         if (char in map) {
-            
             let topElement = stack.length === 0 ? '#' : stack.pop();
             
-
             if (topElement !== map[char]) {
                 return false;
             }
@@ -28,10 +25,8 @@ var isValid = function(s) {
         }
     }
     
-    
     return stack.length === 0;
-}; 
+}
 
 module.exports = { isValid };
-
 
