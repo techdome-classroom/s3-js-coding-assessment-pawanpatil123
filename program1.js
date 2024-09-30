56d9816f0cbd6c1694435890482980738eda8623
piyush patil <piyushpatil44099@gmail.com>
2024-09-30 15:34:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,8 +4,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack =[];
-    let
+    let stack = [];
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        // If it's a closing bracket
+        if (char in map) {
+            // Pop the top of the stack or use a dummy value if stack is empty
+            let topElement = stack.length === 0 ? '#' : stack.pop();
+            
+            // If the popped element doesn't match the expected opening bracket
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+            // It's an opening bracket, push onto the stack
+            stack.push(char);
+        }
+    }
+    
+    // If the stack is empty, the parentheses are balanced
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
